2fe9acb86d94c20c651d42c8bc89e15a7acc3f80
MARIANA BANANA <156617964+yunemaru@users.noreply.github.com>
2024-04-07 23:44:10 -0600

    Add files via upload

---
+++ Mypag.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>MARIANA YUNEIRI</title>  
+</head>
+<body>
+    <!-- SECCION INICIO -->
+    <section class="inicio" id="inicio">
+        <div class="contenido-seccion">
+            <header>
+                <div class="nav-bar" onclick="mostrarOcultarMenu()">
+                    <i class="fa-solid fa-bars"></i>
+                </div>
+                <nav id="nav" class="nav">
+                    <a href="#inicio">Inicio</a>
+                    <a href="#sobremi">Sobre mí</a>
+                    <a href="#habilidades">Habilidades</a>
+                    <a href="#resumen">Resumen</a>
+                    <a href="#contacto">Contacto</a>
+                </nav>
+                <div class="logo">
+                    Yunne<span class="color">CB</span>
+                </div>
+            </header>
+            <div class="info">
+                <h1>MARIANA YUNEIRI CABRERA HERRERA</h1>
+                <h2>ARMY, BIKER Y ESTUDIANTE UNIVERSITARIA</h2>
+                <div class="redes">
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-pinterest-p"></i></a>
+                </div>
+       
+    </section>
+
+    <!-- SECCION SOBRE MI -->
+    <section class="sobremi" id="sobremi">
+        <div class="contenido-seccion">
+            <h2 class="titulo-seccion">Sobre Mi</h2>
+            <h3>Datos Personales</h3>
+
+            <p class="especial">Me gusta el cafe frio , ver series y peliculas en dias libres</p>
+            <p>Agust D is a South Korean rapper, songwriter and record producer. Under Big Hit Entertainment, he debuted as a member of the South Korean boy band BTS in 2013.</p>
+
+            <div class="fila">
+                <div class="col">
+                    <i class="fa-solid fa-user"></i>
+                    <span class="titulo-detalle">Perfil</span>
+                    <p>Min Yoon-gi (Korean: 민윤기; born March 9, 1993), known professionally by his stage names Suga (슈가; stylized in all caps) and Agust D.</p>
+                </div>
+                <div class="col">
+                    <i class="fa-solid fa-location-pin"></i>
+                    <span class="titulo-detalle">Ubicación</span>
+                    <p>Daegu (대구?, DaeguRR, TaeguMR) pronunciado: [tɛ̝.ɡu], oficialmente Ciudad Metropolitana de Daegu (대구광역시?, Daegu-gwangyeoksiRR, Taegu-gwangyŏkshiMR)</p>
+                </div>
+                <div class="col">
+                    <i class="fa-solid fa-flag"></i>
+                    <span class="titulo-detalle">Intereses</span>
+                    <p>Had he not been an idol, Suga would have probably chosen to study architecture or interior design. These careers align with his main interests after music: art and composition. They are areas he would have liked to work in if he had not pursued the K-pop path.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- SECCION HABILIDADES -->
+    <section class="habilidades" id="habilidades">
+        <div class="contenido-seccion">
+            <h2 class="titulo-seccion">Mis habilidades</h2>
+            <h3>Me especializo en</h3>
+
+            <div class="fila">
+                <div class="col">
+                    <span>HTML/CSS</span>
+                    <div class="cont-barra">
+                        <div class="barra" id="html">
+                            <!-- mediante javascript luego genereremos 17 barritas con clase e -->
+                            
+                        </div>
+                        <span>98%</span>
+                    </div>
+                </div>
+                <div class="col">
+                    <span>JAVASCRIPT</span>
+                    <div class="cont-barra">
+                        <div class="barra" id="javascript">
+                            <!-- mediante javascript luego genereremos 17 barritas con clase e -->
+                           
+                        </div>
+                        <span>70%</span>
+                    </div>
+                </div>
+            </div>
+            <div class="fila">
+                
+                    </div>
+                </div>
+            </div>
+                    </div>
+                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- SECCION RESUMEN -->
+    <section class="resumen" id="resumen">
+        <div class="contenido-seccion">
+            <h2 class="titulo-seccion">Resumen</h2>
+            <div class="info">
+                <div class="col">
+                    <span class="titulo">Educación</span>
+                    <table>
+                        <tr>
+                            <td class="datos">
+                            </td>
+                            <td rowspan="2" class="descripcion">
+                            Following revisions made to the Military Service Act in December 2020, all members of BTS were granted an automatic postponement of their mandatory military service in 2021 until the end of 2022.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h4>Ing de Software</h4>
+                                <p>Universidad Politecnica de Lazaro Cardenas</p>
+                            </td>
+                        </tr>
+                    </table>
+                    <table>
+                
+                    </table>
+                </div>
+                <div class="col">
+                    <span class="titulo">Experiencia Laboral</span>
+                    <table>
+                        <tr>
+                            <td class="datos">
+                                <h3>1 Año</h3>
+                            </td>
+                            <td rowspan="2" class="descripcion">
+                            Miembro de la tripulación que tiene como misión principal la vigilancia de la seguridad del vuelo y la comodidad de los pasajeros.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h4>flight attendant</h4>
+                                <p>The chief flight attendant gave us the welcome upon boarding.</p>
+                               
+                            </td>
+                        </tr>
+                    </table>
+                    <table>
+                        <tr>
+                            <td class="datos">
+
+                            </td>
+                            <td rowspan="2" class="descripcion">
+                            Following revisions made to the Military Service Act in December 2020, all members of BTS were granted an automatic postponement of their mandatory military service in 2021 until the end of 2022.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h4>Biker</h4>
+                                <p>Actualmente conduciendo una honda cargo</p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- SECCION CONTACTO -->
+    <section class="contacto" id="contacto">
+        <div class="contenido-seccion">
+            <h2 class="titulo-seccion">Contacto</h2>
+            <h3>Ponte en contacto ahora mismo!</h3>
+
+            <div class="fila">
+                <div class="col">
+                    <h2>
+                        <i class="fa-solid fa-phone"></i> Teléfono
+                    </h2>
+                    <span class="info-contacto">+52 7531576886</span>
+                </div>
+                <div class="col">
+                    <h2>
+                        <i class="fa-solid fa-envelope"></i> Correo
+                    </h2>
+                    <span class="info-contacto">yunmecb@gmail.com</span>
+                </div>
+                <div class="col">
+                    <h2>
+                        <i class="fa-solid fa-globe"></i> Localidad
+                    </h2>
+                    <span class="info-contacto">Lazaro Cardenas Michoacan</span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="app.js"></script>
+</body>
+</html>
